d1c1b63363e587e96d3781fdeeb47d4959976e04
Freddy Aguilar Rojas <freddy.aguilar@et.bo>
2022-07-10 15:57:55 -0400

    Test with out unit test

---
+++ src/main/java/withOutTest/Calculator.java
@@ -0,0 +1,14 @@
+package withOutTest;
+
+/**
+ * @author freddyar
+ */
+public class Calculator {
+
+    public Calculator() {
+    }
+
+    public int suma(int a, int b) {
+        return a + b;
+    }
+}
